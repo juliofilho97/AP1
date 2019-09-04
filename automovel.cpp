27bf0eab3a8674e92d19a60bbbbe87c5ef7dc9ff
Julio Cesar Filho <juliomaiaf@live.com>
2019-09-04 17:08:15 -0300

    Create automovel.cpp

---
+++ automovel.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std
+
+class automovel{
+	private: 
+	string marca; 
+	float preco; 
+	int fab_ano;
+	int fab_dia;
+	int fab_mes;
+
+	public: 
+	string getMarca(); 
+	string setMarca(string m); 
+	float getPreco(); 
+	float setPreco(float p);
+	int getFab_ano(); 
+	int setFab_ano(int fd);
+	int getFab_dia(); 
+	int setFab_dia(int fdi);
+	int getFab_mes(); 
+	int setFab_mes(int fmes);
+}
+
+
+class num_chassi{
+
+//usa numero, caracter e letra
+
+	int main (){
+		cout << endl<< "Digite os dados do automovel:";]
+		cout << endl<<"Marca: ";
+		getline(cin, Marca);
+
+		cout << endl <<"Preço";
+
+
+	}
