b6f890e1873f4cb322110a6ff9e7cd0f167a68e0
Julio Cesar Filho <juliomaiaf@live.com>
2019-09-05 01:21:51 -0300

    Create mainAP.cpp

---
+++ mainAP.cpp
@@ -0,0 +1,13 @@
+#include "automovel.h"
+#include <iostream>
+using namespace std;
+
+int main (){
+	automovel a1;
+	automovel a2;
+		
+		cout <<"Digite os dados do automovel:";]
+		cout << endl<<"Marca: ";
+		getline(cin, Marca);
+
+		cout << endl <<"Preço";
