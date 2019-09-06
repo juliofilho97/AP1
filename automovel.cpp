b20d1008f9108bf847671cc070fdfbf6f96e22fa
Julio Cesar Filho <juliomaiaf@live.com>
2019-09-05 21:28:21 -0300

    Update automovel.cpp

--- automovel.cpp
+++ automovel.cpp
@@ -1,15 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
+
+#include "gerencia.h"
 #include "automovel.h"
+
 using namespace std;
 
 automovel::automovel(){
 		marca = " ";
 		preco = 0;
+		chassi = " ";
 
 	}
 
-automovel::automovel( string marca, float preco){
+automovel::automovel( string marca, float preco, string chassi){
 	setMarca(marca);
 	setPreco(preco);
+	setChassi(chassi);
 }
 
 string automovel::getMarca(){
@@ -19,3 +29,6 @@ float automovel::getPreco(){
 	return preco;
 }
 
+string automovel::getchassi(){
+	return chassi;
+}
