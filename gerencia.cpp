47d1b79938806ed5bd2ef57c96e2e9f316821456
Julio Cesar Filho <juliomaiaf@live.com>
2019-09-05 21:29:00 -0300

    Update gerencia.cpp

--- gerencia.cpp
+++ gerencia.cpp
@@ -1,7 +1,54 @@
+#include <iostream>
 #include <vector>
 #include <string>
-#include <iostream>
+#include <iterator>
+#include <algorithm>
+
 #include "gerencia.h"
 #include "automovel.h"
+
+using namespace std;
+
+
+bool gerencia::add_carro(){
+	string marca;
+	float preco;
+	string chassi;
+
+	cout << endl << "-> Digite os dados do carro : "
+
+	cout << endl << "Marca : ";
+	getline(cin,marca);
+
+	cout << endl << "Preco : ";	
+	preco = get_int();
+
+	cout << endl << "Numero do Chassi : ";
+	getline(cin, chassi);
+
+	automovel *func = new automovel(marca,preco,chassi);
+
+	for (vector<<automovel*> iterator it = listaConc.begin(); it != listaConc.end();it++)
+	{
+		if (**it == *func){
+			cout << endl << "Carro ja cadastrado. Operacao CANCELADA !"<< endl;
+			return false;
+		}
+	}
+	listaConc.push_back(func);
+
+	return true;
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
