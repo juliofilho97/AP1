7fc9f346f50c6c6d36dc4809c95db3e76e02bbdc
Julio Cesar Filho <juliomaiaf@live.com>
2019-09-06 03:12:41 -0300

    Update gerenciar.h

--- gerenciar.h
+++ gerenciar.h
@@ -1,47 +1,37 @@
+#ifndef GERENCIAR_H
+#define GERENCIAR_H
+
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <iterator>
 #include <algorithm>
 
-#include "gerencia.h"
+
 #include "automovel.h"
 
-using namespace std;
-gerencia listaConc
+enum statusConc
+{
+	inexistente = 0,
+	existe = 1,
+	carro_cadastrado = 2
+};
 
-int main (){
-	int x = -1;
-	while (x!= 0) {
-		cout << endl << " Escolha a opcao desejada: "<<endl
-		<< "1 - Adiconar Automovel "<<endl
-		<< " 2- Criar Concessionaria"<< endl
-		<< "3 - Lista de Automoveis"<< endl
-		<<"4- Autualizar uma Concessionaria"<< endl
-		<< " 0 - Sair"<< endl
-		<< endl<< " Digite sua Escolha: ";
-	
-		int x = get_int();
+class gerenciar
+{
+private: 
+	vector <concessionaria*> listaLoja;
 
-		switch (x){
-			case 1: 
-					listaConc.add_carro();
-					break;
-			case 2: 
-					listaConc.criar_conc();
-					break;
-			case 3:
-					listaConc.lista_carro();
-			case 0:
-					cout<<endl<< "Ate mais!"<< endl;
-					return 0;
-			default:
-			cin.clear();
-			cin.ignore(200,'\n');
-			cout << endl << "Entrada invalida, digite novamente"<<endl;
-		}
-	}
-	return 0;
+public:
+	gerenciar();
+	~gerenciar();
 
+	concessionaria criarconcessionaria();
 
-}
+	void cadastrarCarro();
+	bool estoques ();
+
+};
+
+#endif
